ef3afc51c3bf272d9c6fe64547a453196082666c
maryekoko94 <maryekoko94@gmail.com>
2023-05-05 14:14:48 -0700

    1-print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,47 +1,28 @@
+#include <stdio.h>
 #include "main.h"
-/**
- * _power - calculate (base and power)
- * @base: base of the exponent
- * @pow: power of the exponent
- * L0MAX
- * Return: value of base and power
- */
-unsigned long int _power(unsigned int base, unsigned int pow)
-{
-	unsigned long int number;
-	unsigned int edze;
 
-	number = 1;
-	for (edze = 1; edze <= pow; edze++)
-		number *= base;
-	return (number);
-}
 /**
- * print_binary - prints the binary representation of a number
- * @n: num of parented
- * Return: void
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int donas, results;
-	char flag;
+int i;
+unsigned long int current;
+int count = 0;
 
-	flag = 0;
-	donas = _power(2, sizeof(unsigned long int) * 8 - 1);
+for (i = 63; i >= 0; i--)
+{
+current = n >> i;
 
-	while (donas != 0)
-	{
-		results = n & donas;
-		if (results == donas)
-		{
-			flag = 1;
-			putchar('1');
-
-		}
-		else if (flag == 1 || donas == 1)
-		{
-			_putchar('0');
-		}
-		donas >>= 1;
-	}
+if (current & 1)
+{
+putchar('1');
+count++;
+}
+else if (count)
+putchar('0');
+}
+if (!count)
+putchar('0');
 }
